3414c1104c249ab1f207af1c1bf5d20d2b09ab18
Mauricio Andrés Castro Morales <maocastro13@gmail.com>
2023-04-03 12:39:50 -0500

    Commit1

---
+++ Assets/Scripts/EjecicioPrueba1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjecicioPrueba1 : MonoBehaviour
+{
+    int[] ejercicio1 = {10, 20, 30, 40, 50};
+
+    void Start()
+    {
+        for (int i = ejercicio1.Length - 2; i < ejercicio1.Length; i++)
+        {
+            Debug.Log("la respuesta es: " + ejercicio1[i]);
+        }
+    }
+
+}
